e8d6038b5d1560e0a4942e1e898784946734af16
gmp26 <gmp26@cam.ac.uk>
2014-06-05 15:04:56 +0100

    first commit with synth tutorial applied

---
+++ front/js/controllers/tweets.js
@@ -0,0 +1,12 @@
+angular.module('synth').controller('tweetsController', function($scope, $http, data) {
+    $scope.tweets = data.tweets;
+
+    $scope.publish = function() {
+        $http.post('/api/tweets', {
+            content: $scope.newTweet
+        }).success(function(tweet) {
+            $scope.tweets.unshift(tweet);
+        });
+        $scope.newTweet = '';
+    };
+});
